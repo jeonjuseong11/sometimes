4e9d29c4f6382d41867bd49667bc59c3e0cdb34b
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-10-10 19:09:38 +0900

    Refactor : decryptData
    복호화함수 오류 수정

--- src/utils/decryptData.js
+++ src/utils/decryptData.js
@@ -3,7 +3,13 @@ export const decryptData = (encryptedData, key) => {
   if (!encryptedData) {
     return null;
   }
+
   const decodedData = atob(encryptedData);
-  const decryptedData = decodeURIComponent(escape(decodedData)).replace(key, "");
+  const decryptedData = decodeURIComponent(escape(decodedData));
+
+  if (decryptedData.endsWith(key)) {
+    return decryptedData.slice(0, -key.length);
+  }
+
   return decryptedData;
 };
